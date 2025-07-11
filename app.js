69e7e1e3959aa3e8e98f2e3d6fe37ac92b24e56f
meetmandawat13 <meetmandawat7@gmail.com>
2025-07-12 00:56:21 +0530

    Ready for deployment

--- app.js
+++ app.js
@@ -69,6 +69,8 @@ io.on("connection", function(uniquesocket){
     })
 })
 
-server.listen(3000, function(){
-    console.log("Listening on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`);
+});
